a6b7af02f9c63ab7dff4fbbe631e3dee6b284e1d
phizzl3 <phizzl3@gmail.com>
2021-05-21 22:43:33 -0500

    Update convert.py Update main function with a lot of corrections to the file processing loop.

--- flac2mp3/convert.py
+++ flac2mp3/convert.py
@@ -3,10 +3,11 @@
 import dropfile
 import re_file_search
 import subprocess
+import time
 
 
 def get_folder():
-    return str(dropfile.get())
+    return dropfile.get()
 
 
 def get_source_files(sf):
@@ -14,18 +15,37 @@ def get_source_files(sf):
 
 
 def convert_files(fl):
+    
+    # Loop through each file in the list.
     for each in fl:
-        subprocess.run(f"ffmpeg -i {each} -ab 320k {each.parent}{each.name}.mp3",
-                       shell=True)
+        
+        # Set a new variable for the source and clean characters that need
+        # to be escaped for MacOS/Linux.
+        source = str(each)
+        for fnd, rplc in {" ": "\ ", "'": "\\'", "(": "\\(", ")": "\\)",
+                          "[": "\\[", "]": "\\]", "{": "\\{", "}": "\\}",
+                          }.items():
+            source = source.replace(fnd, rplc)
+        
+        # Set a target filename by just replacing the extension.
+        target = source.replace(".flac", ".mp3")
+        
+        # Send the terminal command to ffmpeg to do the conversion.
+        subprocess.run(f"ffmpeg -i {source} -ab 320k {target}", shell=True)
 
 
 def main():
     source_folder = get_folder()
     flac_list = get_source_files(source_folder)
     if flac_list:
+        print(f"\n{len(flac_list)} files found.\n")
+        time.sleep(1)
         convert_files(flac_list)
+        print("\nAll done.")
+        time.sleep(3)
     else:
-        print("No matching files found.")
+        print("\nNo matching files found.\nDouble check that your path"
+              " matches when dropped. (check extra characters, etc.)")
 
 
 if __name__ == '__main__':
